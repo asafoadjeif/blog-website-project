cd181af7647c55af3b6fe2381b1ba40831f4af74
Hayden Kidd <hkidd@live.co.uk>
2021-12-13 15:46:44 +0000

    added controller and models functions

--- Server/models/post.js
+++ Server/models/post.js
@@ -8,9 +8,28 @@ class Post{
         this.reactions = {thumbsUp: 0, thumbsDown: 0, heart: 0};
         this.comments = [];
     }
+
     static get all(){
         return postsData;
     }
+
+    static findById(id){
+        const postData = postsData.filter((post) => post.id === id)[0];
+        const post = new Post(postData);
+        return post;
+    };
+
+    static add(post) {
+        const newPostId = postsData[postsData.length + 1].id + 1;
+        const newPost = new Post({ id: newPostId, ...post });
+        postsData.push(newPost);
+        return newPost;
+    };
+
+    delete() {
+        const post = postsData.filter((post) => post.id === this.id)[0];
+        postsData.splice(postsData.indexOf(post), 1);
+    };
 };
 
 module.exports = Post;
