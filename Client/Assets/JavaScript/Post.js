8e6e61f2eb3734b2a797ace6b68bfbadb914ff18
Franklyn Asafo-Adjei <asafoadjeif@gmail.com>
2021-12-15 13:16:20 +0000

    added react buttons

--- Client/Assets/JavaScript/Post.js
+++ Client/Assets/JavaScript/Post.js
@@ -1,14 +1,16 @@
 // const { add } = require("../../../Server/models/post");
 
-const postList = document.getElementById('postList');
-const apiDomain = 'http://localhost:3000/';
+// const res = require("express/lib/response");
 
+const postList = document.getElementById('postList');
+const apiDomain = 'https://fp-snappy.herokuapp.com/';
+const gifDisplay = document.getElementById('gifResults');
 postBtn = document.getElementById('postBtn');
 gifBtn = document.getElementById('gifBtn');
-
-
+let imgSource;
 
 //SCRIPT FUNCTIONS TO ALTER WEBPAGE
+
 
 
 // ******************************
@@ -25,7 +27,9 @@ gifBtn.addEventListener('click', (e) => {
   fetch(`${apiDomain}giphy/${search}`)
     .then((response) => response.json())
     .then((obj) => {
+    
       const gifDisplay = document.getElementById("gifResults");
+      console.log(obj);
 
       for (let i = 0; i < obj.length; i++) {
         // Create an image and set its source to the current image
@@ -37,11 +41,10 @@ gifBtn.addEventListener('click', (e) => {
         // Add an event listener to each photo
         tempImg.addEventListener("click", (e) => {
           // store the source of the clicked image
-          const imgSource = e.target.src;
+          imgSource = e.target.src;
 
           // close the popup when image is selected
-          document.getElementById("gifPopup").style.display = "none";
-
+          $('#gifs').collapse({toggle: false})
           // Add it to the dom
           if (document.getElementById('gifToAdd')) {
             document.getElementById('gifToAdd').remove()
@@ -69,14 +72,12 @@ gifBtn.addEventListener('click', (e) => {
 // ******************************
 
 postBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    message = document.querySelector("#textBox").value;
+  e.preventDefault();
+  let message = document.querySelector("#textBox").value;
     
 data = {
     text: message,
-    giphyUrl: 'giphyUrl',
-    reactions: 'reactions',
-    comments: 'comments',
+    giphyUrl:`${imgSource}`,
 }
 
 
@@ -88,7 +89,7 @@ const options = {
         body: JSON.stringify(data),
       };
 
-    fetch(`http://localhost:3000/posts`, options)
+    fetch(`${apiDomain}posts`, options)
     .then((response) => response.json())
     .then((obj) => {
       console.log(obj)
@@ -131,14 +132,18 @@ const options = {
 
 
 function loadContent(){
-
-
+function removeAllChildNodes() {
+    while (postList.firstChild) {
+        postList.removeChild(postList.firstChild);
+    }
+}
+removeAllChildNodes();
 fetch(`${apiDomain}posts`)
     .then((response) => response.json())
     .then((obj) => {
       const postDisplay = document.getElementById("postList");
 
-      for (let i = 0; i < obj.length; i++) {
+      for (let i = obj.length - 1; i > -1; i--) {
 
       
             // create a card to show posts 
@@ -170,21 +175,65 @@ fetch(`${apiDomain}posts`)
             postCard.append(emojiBar);
             postList.append(postCard);
 
-            // create buttons and their IDs
+            // create buttons and their IDs / classes
             const react1 = document.createElement('button');
             const react2 = document.createElement('button');
             const react3 = document.createElement('button');
             const commentBtn = document.createElement('button');
 
-            react1.setAttribute('id', 'like');
-            react2.setAttribute('id', 'dislike');
-            react3.setAttribute('id', 'funny');
-            commentBtn.setAttribute('id', 'comment');
+            react1.setAttribute('id', `like:${obj.id}`);
+            react2.setAttribute('id', `dislike:${obj.id}`);
+            react3.setAttribute('id', `heart:${obj.id}`);
+            commentBtn.setAttribute('id', `comment:${obj.id}`);
+
+            react1.classList.add('like');
+            react2.classList.add('dislike');
+            react3.classList.add('heart');
+            commentBtn.classList.add('comment');
+
+            react1.classList.add('reactBtn');
+            react2.classList.add('reactBtn');
+            react3.classList.add('reactBtn');
+            commentBtn.classList.add('reactBtn');
 
             emojiBar.append(react1);
             emojiBar.append(react2);
             emojiBar.append(react3);
             emojiBar.append(commentBtn);
+
+            // add event listener to each button 
+            
+
+            react1.addEventListener('click', e => {
+              e = {emojiId: 0};
+              options = {
+                method: 'PATCH',
+                headers: {'Content-Type' : 'application/JSON'},
+                body: JSON.stringify(e),
+              
+              }
+              fetch(`${apiDomain}posts/reactions/${obj[i].id}`, options)
+            })
+
+            react2.addEventListener('click', e => {
+              e = {emojiId: 1};
+              options = {
+                method: 'PATCH',
+                headers: {'Content-Type' : 'application/JSON'},
+                body: JSON.stringify(e)
+              }
+              fetch(`${apiDomain}posts/reactions/${obj[i].id}`, options)
+            })
+
+            react3.addEventListener('click', e => {
+              e = {emojiId: 2};
+              options = {
+                method: 'PATCH',
+                headers: {'Content-Type' : 'application/JSON'},
+                body: JSON.stringify(e)
+              }
+              fetch(`${apiDomain}posts/reactions/${obj[i].id}`, options)
+            })
 
 
 
@@ -198,13 +247,14 @@ fetch(`${apiDomain}posts`)
 
 
 
-// script functions to fetch data from server
-let port = 3000;
-function getPost () {
-    fetch(`http://localhost:3000/posts`).
-    then( r => r.json()).
-    then(data => console.log(data))
-}
 
-getPost();
+// script functions to fetch data from server
+// let port = 3000;
+// function getPost () {
+//     fetch(``).
+//     then( r => r.json()).
+//     then(data => console.log(data))
+// }
+
+// getPost();
 loadContent();
